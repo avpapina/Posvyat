8b47ba0ff5a35298a0d6fae67a3b2b1df2736fb7
Папина Анжелика Владимировна <avpapina@edu.hse.ru>
2024-08-13 17:59:41 +0300

    исправлена валидация данных, добавлен corse

---
+++ posvyat/apps/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Registration(models.Model):
+
+    name = models.CharField(max_length=100, blank = False)
+    surname = models.CharField(max_length=100, blank = False)
+    middle_name = models.CharField(max_length=100, blank= False)
+
+    vk = models.CharField(max_length=100, blank=False)
+    tg = models.CharField(max_length=100, validators=[RegexValidator(r'^@.+')], blank= False)
+
+    phone = PhoneNumberField(blank=False)
+    university = models.CharField(max_length=300, blank=False)
+    faculty = models.CharField(max_length=300, blank=False)
+
+    class Transfer(models.TextChoices):
+
+        ODINTSOVO = 'OD', 'Да, от Одинцово и обратно'
+        PARKPOBEDY = 'PP', 'Да, от Парка Победы и обратно'
+        NO = 'NO', 'Не нужен'
+
+    transfer = models.CharField(
+        max_length=2,
+        choices=Transfer.choices,
+        default=Transfer.NO,
+    )
+
+    course_choices = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    course = models.IntegerField(
+        choices=course_choices,
+        default=1,
+    )
+
+    health_features = models.TextField(blank=True)
+
+
--- posvyat/posvyat/settings.py
+++ posvyat/posvyat/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'apps',
     'phonenumber_field.formfields',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'posvyat.urls'
@@ -123,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
