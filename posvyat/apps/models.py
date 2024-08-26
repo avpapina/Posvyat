6938b3ab23d9c423367cd6b891ec82955bf42da9
Папина Анжелика Владимировна <avpapina@edu.hse.ru>
2024-08-26 22:54:38 +0300

    добавлена модель трансфера и расселения

--- posvyat/apps/models.py
+++ posvyat/apps/models.py
@@ -16,7 +16,7 @@ class Registration(models.Model):
     university = models.CharField(max_length=300, blank=False)
     faculty = models.CharField(max_length=300, blank=False)
 
-    class Transfer(models.TextChoices):
+    class forTransfer(models.TextChoices):
 
         ODINTSOVO = 'OD', 'Да, от Одинцово и обратно'
         PARKPOBEDY = 'PP', 'Да, от Парка Победы и обратно'
@@ -24,8 +24,8 @@ class Registration(models.Model):
 
     transfer = models.CharField(
         max_length=2,
-        choices=Transfer.choices,
-        default=Transfer.NO,
+        choices=forTransfer.choices,
+        default=forTransfer.NO,
     )
 
     course_choices = [
@@ -43,4 +43,44 @@ class Registration(models.Model):
 
     health_features = models.TextField(blank=True)
 
+class Transfer(models.Model):
+
+    name = models.CharField(max_length=100, blank = False)
+    surname = models.CharField(max_length=100, blank = False)
+    middle_name = models.CharField(max_length=100, blank= False)
+
+    email = models.EmailField(max_length=254, blank=False)
+    vk = models.CharField(max_length=100, blank=False)
+    tg = models.CharField(max_length=100, validators=[RegexValidator(r'^@.+')], blank= False)
+    phone = PhoneNumberField(blank=False)
+
+
+class Rasselenie(models.Model):
+
+    name = models.CharField(max_length=100, blank = False)
+    surname = models.CharField(max_length=100, blank = False)
+    middle_name = models.CharField(max_length=100, blank= False)
+
+    sex = models.CharField(max_length=2, choices=(('W', 'Women'), ('M', 'Men')), blank=False)
+    vk = models.CharField(max_length=100, blank=False)
+    tg = models.CharField(max_length=100, validators=[RegexValidator(r'^@.+')], blank= False)
+
+    faculty = models.CharField(max_length=300, blank=False)
+    group = models.CharField(max_length=50, blank=False)
+    course_choices = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    course = models.IntegerField(
+        choices=course_choices,
+        default=1,
+    )
+
+class ListNames(models.Model):
+        listname = models.ForeignKey(Rasselenie, related_name='values')
+        personname = models.CharField(max_length=200, blank=True)
 
