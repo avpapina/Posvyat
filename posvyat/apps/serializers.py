68ba3fe460e0b4f799ff20f7d632d50ebf234e5b
alexotl777 <alexotl228@yandex.ru>
2024-08-29 16:09:47 +0300

    feat: added POST transfer

--- posvyat/apps/serializers.py
+++ posvyat/apps/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 
-from apps.models import Registration
-
+from apps.models import Registration, Transfer
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Registration
+        fields = '__all__'
+
+
+class TransferSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transfer
         fields = '__all__'
--- posvyat/apps/urls.py
+++ posvyat/apps/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from apps.views import RegistrationAPI
+from apps.views import RegistrationAPI, TransferAPI
 
 urlpatterns = [
     path('', views.start_page, name='start_page'),
     path('main_page/', views.main_page, name='main_page'),
-    path('api/v1/registration', RegistrationAPI.as_view(), name='registration')
+    path('api/v1/registration', RegistrationAPI.as_view(), name='registration'),
+    path('api/v1/transfer', TransferAPI.as_view(), name='transfer')
 ]
